1f181e9653c45c65f0c0644df513d89d94437a21
unknown <softbala92@gmail.com>
2025-01-01 21:12:56 +0530

    01-01-2024

--- Project1/app.py
+++ Project1/app.py
@@ -1,7 +1,23 @@
-from flask import Flask,jsonify,request,redirect,url_for,session,render_template
+from flask import Flask,jsonify,request,redirect,url_for,session,render_template,g
+import sqlite3
 app = Flask(__name__,template_folder='template')
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = "THISISMYSECRETKEY"
+DATABASE_PATH = 'database.db'
+
+def connect_db():
+    sql=sqlite3.connect(DATABASE_PATH)
+    sql.row_factory = sqlite3.Row
+    return sql
+def get_db():
+    if not hasattr(g,'sqlite3'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g,'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route("/")
 def index():
@@ -56,6 +72,16 @@ def processjson():
                     'name':name,
                     'location':location,
                     'randomlist':randomlist[1]})
+
+# db work
+@app.route("/viewresults")
+def viewresults():
+    db  = get_db()
+    print(db)
+    cur=db.execute("select id,name,location  from user ")
+
+    results = cur.fetchall()
+    return "<h1>Id {} name{} location {}</h1>".format(results[0]['id'],results[0]['name'],results[0]['location'])
 if __name__ == "__main__":
     app.run()
     
